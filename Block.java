cf7e2e88cce34be53c24037347375d664166b677
JeromeTheFluffy <noah_gray@phrd.ab.ca>
2014-11-14 15:44:01 -0700

    Block file
    Parent class Block

---
+++ Block.java
@@ -0,0 +1,12 @@
+public class Block{
+  public Block{
+    public string blockName;
+    protected boolean isExplosive;
+    public int hardness;
+    public Block blockBeingBroken;
+    }
+  public static void mine(Tool tool, Block itemDropped){
+    if(tool.toolName == "Diamond Pickaxe" && Block.blockBeingBroken == ){
+    }
+  }
+}
